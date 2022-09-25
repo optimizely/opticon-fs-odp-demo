4773f19907930760c09896f55420ce08e5da7282
Peter Koomen <koomen@gmail.com>
2022-09-25 10:40:58 -0700

    Hide banner by default

--- demo.js
+++ demo.js
@@ -1,5 +1,10 @@
 console.log("demo.js loaded");
 
+
+/**
+ *  Initialize the Flags SDK
+ */
+
 const OPTIMIZELY_SDK_KEY = "3DHbmsE3z3y3Fb1qmexbA";
 
 const optimizelyClient = window.optimizelySdk.createInstance({
@@ -11,3 +16,46 @@ optimizelyClient.onReady().then(() => {
     window.optimizelyClient = optimizelyClient;
     console.log(optimizelyClient);
 });
+
+
+/**
+ * Instrument the header banner using a Flag
+ */
+
+
+const BANNER_SELECTOR = ".top-header__banner-text p";
+
+// Hack. The banner is displayed by default, so we hide it and then display it according to the flag settings
+waitForElm(BANNER_SELECTOR).then((banner) => {
+    banner.style.visibility = "hidden";
+});
+
+
+
+
+
+
+
+
+
+// Use mutationovbservers to wait for a dom element to be loaded
+// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
