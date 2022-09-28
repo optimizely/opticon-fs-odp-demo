f1003e857e650228f40eb7fdd425e574b416bed5
Peter Koomen <koomen@gmail.com>
2022-09-28 11:39:52 -0700

    Cleanup header script and add a zaius listener

--- demo_header.js
+++ demo_header.js
@@ -1,4 +1,4 @@
-const VERSION = "0.0.6";
+const VERSION = "0.0.7";
 console.log(`demo_header.js loaded (v${VERSION})`);
 
 
@@ -12,105 +12,14 @@ const optimizelyClient = window.optimizelySdk.createInstance({
     sdkKey: OPTIMIZELY_SDK_KEY
 });
 
-optimizelyClient.onReady().then(() => {
-    console.log("Optimizely client is ready");
-    window.optimizelyClient = optimizelyClient;
-    console.log(optimizelyClient);
-});
-
-
-/**
- * Instrument the header banner using a Flag
- */
-
-const BANNER_SELECTOR = ".top-header__banner-text";
-const BANNER_TEXT_SELECTOR = ".top-header__banner-text p";
-
-function renderBanner(enabled, {
-    banner_text = "Welcome to Mosey!",
-    discount_pct = 0,
-    banner_background_color = "black",
-    banner_text_color = "white",
-}) {
-    waitForElm(BANNER_SELECTOR).then((banner) => {
-        const text = document.querySelector(BANNER_TEXT_SELECTOR);
-
-        if (enabled) {
-            console.log("Rendering banner");
-            text.innerHTML = banner_text.replace("$discount_pct", discount_pct);
-            text.style.color = banner_text_color;
-            banner.style.backgroundColor = banner_background_color;
-            banner.style.visibility = "visible";
-        } else {
-            console.log("Hiding banner");
-            banner.style.visibility = "hidden";
-        }
-
-    });
-}
-
-
-docReady().then(() => {
-
-    // Hack. The banner is displayed by default, so we hide it and then display it according to the flag settings
-    // renderBanner(false, {});
-
-    // optimizelyClient.onReady().then(() => {
-    //     const userCtx = window.optimizelyClient.createUserContext("user123");
-
-    //     const decision = userCtx.decide("mosey_banner");
-
-    //     console.log(decision.variables);
-
-    //     renderBanner(decision.enabled, decision.variables);
-    // });
-});
 
 /**
  * Instrument hero image using a flag
  */
 
-
-const HERO_CONTAINER_SELECTOR = "#heroBlock-174"
-const HERO_IMAGE_SELECTOR = ".hero-block__image";
-const HERO_H1_SELECTOR = ".hero-block__callout-content h1";
-const HERO_H3_SELECTOR = ".hero-block__callout-content h3";
-const HERO_BUTTON_SELECTOR = ".hero-block__callout-content a";
-
-
-function renderHero(enabled, {
-    image_url = "/globalassets/_mosey/start/woman1-large.png",
-    h1_text = "Sustainable Clothing",
-    h3_text = "Make a difference",
-    button_text = " Learn more ",
-    button_url = "/en/fashion/mens/"
-}) {
-
-    waitForElm(HERO_CONTAINER_SELECTOR).then((hero) => {
-
-        if (enabled) {
-            const heroImage = hero.querySelector(HERO_IMAGE_SELECTOR);
-            heroImage.style.backgroundImage = `url(${image_url})`;
-
-            const h1 = hero.querySelector(HERO_H1_SELECTOR);
-            h1.innerHTML = h1_text;
-
-            const h3 = hero.querySelector(HERO_H3_SELECTOR);
-            h3.innerHTML = h3_text;
-
-            const button = hero.querySelector(HERO_BUTTON_SELECTOR);
-            button.innerHTML = button_text;
-            button.href = button_url;
-
-            hero.style.display = "block";
-        } else {
-            hero.style.display = "none";
-        }
-
-    });
-
-
-}
+odpReady().then(() => {
+    console.log("window.zaius is ready");
+});
 
 
 
@@ -146,10 +55,52 @@ docReady().then(() => {
  * Library functions
  */
 
+/**
+ * renderHero
+ * @param {boolean} enabled     true if the hero element should be displayed 
+ * @param {*} param1            a set of variables that control the appearance of the hero offer 
+ */
+function renderHero(enabled, {
+    image_url = "/globalassets/_mosey/start/woman1-large.png",
+    h1_text = "Sustainable Clothing",
+    h3_text = "Make a difference",
+    button_text = " Learn more ",
+    button_url = "/en/fashion/mens/"
+}) {
+
+    const HERO_CONTAINER_SELECTOR = "#heroBlock-174"
+    const HERO_IMAGE_SELECTOR = ".hero-block__image";
+    const HERO_H1_SELECTOR = ".hero-block__callout-content h1";
+    const HERO_H3_SELECTOR = ".hero-block__callout-content h3";
+    const HERO_BUTTON_SELECTOR = ".hero-block__callout-content a";
+
+    waitForElm(HERO_CONTAINER_SELECTOR).then((hero) => {
+
+        if (enabled) {
+            const heroImage = hero.querySelector(HERO_IMAGE_SELECTOR);
+            heroImage.style.backgroundImage = `url(${image_url})`;
+
+            const h1 = hero.querySelector(HERO_H1_SELECTOR);
+            h1.innerHTML = h1_text;
+
+            const h3 = hero.querySelector(HERO_H3_SELECTOR);
+            h3.innerHTML = h3_text;
+
+            const button = hero.querySelector(HERO_BUTTON_SELECTOR);
+            button.innerHTML = button_text;
+            button.href = button_url;
+
+            hero.style.display = "block";
+        } else {
+            hero.style.display = "none";
+        }
+
+    });
 
 
+}
 
-// Returns a promise that resolves when the document is ready
+
 /**
  * docReady
  * @returns a Promise that resolves when document.body is ready
@@ -158,6 +109,25 @@ function docReady() {
     return new Promise((resolve, reject) => {
         const interval = setInterval(() => {
             if (document.body) {
+                resolve();
+                clearInt();
+            }
+        }, 10);
+
+        const clearInt = () => {
+            clearInterval(interval);
+        }
+    });
+}
+
+/**
+ * zaiusReady
+ * @returns a Promise that resolves when window.zaius is ready
+ */
+function odpReady() {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            if (window.zaius) {
                 resolve();
                 clearInt();
             }
